6c15c2d87d30455908a741f5f029b2db2eed1628
Lee Faria <>
2019-11-03 13:21:10 -0500

    cleaned up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
-//import IPFS from './ipfs.js'
 import "./App.css";
 import ipfs from "./ipfs.js";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null, ipfsHash : null , buffer:null };
+  state = {
+    storageValue: 0,
+    web3: null,
+    accounts: null,
+    contract: null,
+    ipfsHash: null,
+    buffer: null
+  };
 
   componentDidMount = async () => {
     try {
@@ -21,7 +27,7 @@ class App extends Component {
       const deployedNetwork = SimpleStorageContract.networks[networkId];
       const instance = new web3.eth.Contract(
         SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+        deployedNetwork && deployedNetwork.address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
@@ -30,7 +36,7 @@ class App extends Component {
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
@@ -49,50 +55,45 @@ class App extends Component {
     this.setState({ storageValue: response });
   };
 
-  convertToBuffer = async(reader) => {
+  //helper function
+  convertToBuffer = async reader => {
     const buffer = await ipfs.Buffer.from(reader.result);
-    this.setState({buffer});
-    console.log(buffer)
+    this.setState({ buffer });
+    console.log(buffer);
   };
-  
- //converts file submitted to a buffer
- captureFile = (event) =>{
-   event.stopPropagation()
-   event.preventDefault()
-   const file = event.target.files[0]
-   let reader = new window.FileReader()
-   reader.readAsArrayBuffer(file)
-   reader.onloadend = () => this.convertToBuffer(reader)
-   //this.setState({buffer:reader})
- };
 
- //helper function
+  //converts file submitted to a buffer
+  captureFile = event => {
+    event.stopPropagation();
+    event.preventDefault();
+    const file = event.target.files[0];
+    let reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => this.convertToBuffer(reader);
+  };
 
-  
-
-  ipfsSubmit = async (event) => {
+  //submits buffer to ipfs node
+  ipfsSubmit = async event => {
     event.preventDefault();
 
     const accounts = this.state.accounts;
     const buffer = this.state.buffer;
-    console.log('Sending from Metamask account: ' + accounts[0]);
-    console.log(buffer)
+    console.log("Sending from Metamask account: " + accounts[0]);
+    console.log(buffer);
 
     await ipfs.add(this.state.buffer, (err, ipfsHash) => {
-      console.log(err,ipfsHash);
+      console.log(err, ipfsHash);
       this.setState({ ipfsHash: ipfsHash[0].hash });
-      console.log(this.state.ipfsHash)
-    })
+      console.log(this.state.ipfsHash);
+    });
   };
 
-  
-  
   render() {
     if (!this.state.web3) {
       //console.log(this.state.buffer)
       return <div>Loading Web3, accounts, and contract...</div>;
     }
-    console.log(this.state.buffer)
+    console.log(this.state.buffer);
     return (
       <div className="App">
         <h1>Good to Go!</h1>
@@ -106,16 +107,21 @@ class App extends Component {
           Try changing the value stored on <strong>line 40</strong> of App.js.
         </p>
         <form onSubmit={this.ipfsSubmit}>
-        <input type="file" onChange = {this.captureFile}/>
-        <button type="submit" > Submit</button>
+          <input type="file" onChange={this.captureFile} />
+          <button type="submit"> Submit</button>
         </form>
         <p>
-          */run ipfs daemon in a seperate terminal window/*
-          Try uploading a file to IPFS while you have an IPFS daemon running!!
-          If you see the IPFS hash in the console then the file was upload successfully!
+          */run ipfs daemon in a seperate terminal window/* Try uploading a file
+          to IPFS while you have an IPFS daemon running!! If you see the IPFS
+          hash in the console then the file was upload successfully!
         </p>
-        <div>The stored value is: {this.state.storageValue} the ipfs version is {this.state.ipfshash}</div>
-       <a href={"https://gateway.ipfs.io/ipfs/" + this.state.ipfsHash}>Click to see on IPFS. </a>
+        <div>
+          The stored value is: {this.state.storageValue} the ipfs version is{" "}
+          {this.state.ipfshash}
+        </div>
+        <a href={"https://gateway.ipfs.io/ipfs/" + this.state.ipfsHash}>
+          Click to see on IPFS.{" "}
+        </a>
       </div>
     );
   }
